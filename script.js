86cd87ce024ca14b38a2380854263bf2af69512b
lacnoskillz <kaikane3457@hotmail.com>
2022-11-07 17:40:25 -0600

    made a header for highscore div. changed the names of some of the functions to be less confusing. added more detailed notes in java

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 
-
-
-var grabber = document.getElementById("namegraber1");
-grabber.setAttribute('class', 'hide');
-
 //makes "answerbox invisible"
-hideanswerbox();
 function hideanswerbox() {
     element = document.querySelector('.answerbox');
     element.style.visibility = 'hidden';
 
 
 }
+hideanswerbox();
 //declaring variables and arrays
+var grabberEL = document.getElementById("namegraber1");
+var highscoreEL = document.getElementById("highscore");
 var h1 = document.getElementById("toptext");
-var playername = document.getElementById("namegraber1");
-var hr = document.getElementById("line");
-var pp = document.getElementById("pee");
+//var hr = document.getElementById("line");
+var ppEL = document.getElementById("pee");
 var boxA = document.getElementById("buttonA");
 var boxB = document.getElementById("buttonB");
 var boxC = document.getElementById("buttonC");
@@ -24,23 +20,29 @@ var boxD = document.getElementById("buttonD");
 var questionasked = document.getElementById("bottomtext");
 var btnbox = document.getElementById(".button-center");
 var btn = document.querySelector(".START")
-testanswer0 = ["2", "0", "3", "99"];
-testanswer1 = ["mars", "venus", "earth", "sun"];
-testanswer2 = ["1", "29", "70", "24"];
-testanswer3 = ["snake", "owl", "guitar", "dog"];
-questions = ["what is 1+1?", "what planet are we on?", "how old am i?", "which animal has four legs?"];
+testanswer0 = ["Declaration statements", "Keywords", "Data types", "Prototypes"];
+testanswer1 = ["Global variable", "Both A and C", "The local element", "None of the above"];
+testanswer2 = ["Both x and y are equal in value, type and reference address as well.", "Both are equal in the value and data type", "Both are x and y are equal in value only.", "Both are not same at all."];
+testanswer3 = ["if(a!=null)", "if(a!null)", "if (a!)", "if(a!==null)"];
+questions = ["The 'function' and 'var' are known as:", "Which of the following variables takes precedence over the others if the names are the same?", "In JavaScript the x===y statement implies that:", "Choose the correct snippet from the following to check if the variable 'a' is not equal the 'NULL':"];
 var score = 0
 var z = 0;
-originalquestion = questionasked.innerHTML;
+//hides form "namegraber"
+grabberEL.setAttribute('class', 'hide');
+//hides "highscore" 
+highscoreEL.style.visibility = "hidden";
 //runs this game on start click
 function startgame() {
-    //hides h1 and start button upon click
+    //resets score and z variables. z is used for set question function
     z = 0;
     score = 0;
+    //hides h1 and start button upon click
     h1.style.visibility = "hidden";
     btn.disabled = true;
     const startScreen = document.getElementById('start')
     startScreen.classList.add('hide')
+    //hides score list on game start
+    highscoreEL.style.visibility = "hidden";
 
     //makes "answerbox in HTML visible on click of start button"
     element = document.querySelector('.answerbox');
@@ -51,7 +53,7 @@ function startgame() {
 
     var timeEl = document.querySelector(".time");
 
-    var secondsLeft = 20;
+    var secondsLeft = 35;
 
     function setTime() {
 
@@ -70,7 +72,7 @@ function startgame() {
 
     function sendMessage() {
         timeEl.textContent = "FINISHED";
-        highscore();
+        displayform();
 
 
     }
@@ -80,7 +82,7 @@ function startgame() {
     setquestion();
     //function to go through array questions and make answers display in the "answerbox" depending on which question is asked
     function setquestion() {
-        
+
         if ((z > 3) || (secondsLeft <= 0)) {
 
             hideanswerbox();
@@ -155,10 +157,9 @@ function startgame() {
     boxA.addEventListener("click", function () {
 
         if ((z == 0) && (secondsLeft > 0)) {
-            console.log("z", z);
             score++;
             z++;
-            pp.textContent = "Correct!";
+            ppEL.textContent = "Correct!";
             console.log("correct! score:", score);
             setquestion();
 
@@ -166,7 +167,7 @@ function startgame() {
             if (secondsLeft > 0) {
                 secondsLeft = secondsLeft - 5;
                 z++;
-                pp.textContent = "Incorrect!";
+                ppEL.textContent = "Incorrect!";
                 setquestion();
             }
         }
@@ -177,14 +178,14 @@ function startgame() {
             score++;
             z++;
 
-            pp.textContent = "Correct!";
+            ppEL.textContent = "Correct!";
             console.log("correct! score:", score);
             setquestion();
         } else {
             if (secondsLeft > 0) {
                 secondsLeft = secondsLeft - 5;
                 z++;
-                pp.textContent = "Incorrect!";
+                ppEL.textContent = "Incorrect!";
                 setquestion();
             }
         }
@@ -194,14 +195,14 @@ function startgame() {
         if ((z == 1) && (secondsLeft > 0)) {
             score++;
             z++;
-            pp.textContent = "Correct!";
+            ppEL.textContent = "Correct!";
             console.log("correct! score:", score);
             setquestion();
         } else {
             if (secondsLeft > 0) {
                 secondsLeft = secondsLeft - 5;
                 z++;
-                pp.textContent = "Incorrect!";
+                ppEL.textContent = "Incorrect!";
                 setquestion();
             }
         }
@@ -211,14 +212,14 @@ function startgame() {
         if ((z == 3) && (secondsLeft > 0)) {
             score++;
             z++;
-            pp.textContent = "Correct!";
+            ppEL.textContent = "Correct!";
             console.log("correct! score:", score);
             setquestion();
         } else {
             if (secondsLeft > 0) {
                 secondsLeft = secondsLeft - 5;
                 z++;
-                pp.textContent = "Incorrect!";
+                ppEL.textContent = "Incorrect!";
                 setquestion();
             }
         }
@@ -235,63 +236,75 @@ function startgame() {
 
 }
 
-
-
-
 //starts game on start button click
 btn.addEventListener("click", function () {
     startgame();
 
 
 });
-
-function highscore() {
-    //reset pp text to blank
-    pp.textContent = "";
+//function that makes the form "namegraber" from html visible
+function displayform() {
+    //reset ppEL text to blank
+    ppEL.textContent = "";
+    //makes questionsasked text content display users score
     questionasked.textContent = ("Your score: " + score);
 
 
-
-    grabber.removeAttribute('class');
-    grabber.setAttribute('class', 'namegraber');
+    // makes the form element with id "namegraber1" in html visible
+    grabberEL.removeAttribute('class');
+    grabberEL.setAttribute('class', 'namegraber');
 
 }
-//trying to make highscore list
-grabber.addEventListener("submit", getname);
+//listener that runs when user enters initials in form "namegraber" from html and clicks submit
+grabberEL.addEventListener("submit", getname);
+
 function getname(event) {
-event.preventDefault();
+    event.preventDefault();
     userinput = document.getElementById("name");
 
-
+//makes sure user input is 1-3 char long
     var player;
     player = userinput.value
     if ((player.length > 3) || (player.length == 0)) {
         alert("initials must be 1-3 charecters");
-        userinput.value = 0;
+        userinput.value = "";
         return;
-    } 
-    //localStorage.setItem("playerscore and initials", JSON.stringify(player));
-listmaker();
+    }
+//stores in local storage
+    var storedHighscores = JSON.parse(localStorage.getItem('highscores')) || [];
+    var currentHighScore = {
+        name: player,
+        score: score
+    };
+    storedHighscores.push(currentHighScore);
+    localStorage.setItem('highscores', JSON.stringify(storedHighscores));
+
+    //runs function listmaker below
+    listmaker();
 
 
+//creats a <ul> in "highscore" div in HTML and makes <li> items with user name and score
+    function listmaker() {
+        //makes form "namegrabber" from html not visible again
+        grabberEL.setAttribute('class', 'hide');
 
-function listmaker() {
-    //makes form namegrabber visible
-    var grabber = document.getElementById("namegraber1");
-grabber.setAttribute('class', 'hide');
-    //creates <ul> <li> for user input and score then displays it
-    scorelist = document.getElementById("highscore");
-    var listEl = document.createElement("ul");
-    scorelist.appendChild(listEl);
-    var li1 = document.createElement("li");
-    li1.textContent = (player + " score: " + score);
-    listEl.appendChild(li1);
-    
-    questionasked.textContent = ("press start to play again");
-    btn.disabled = false;
-    const startScreen = document.getElementById('start')
-    startScreen.classList.remove('hide')
-  
+        //grabs element "highscore" in html and creats list with player name and score
+        highscoreEL = document.getElementById("highscore");
+        var listEl = document.createElement("ul");
+        
+        highscoreEL.appendChild(listEl);
+        
+        var li1 = document.createElement("li");
+        li1.textContent = (player + " - " + score);
+        listEl.appendChild(li1);
+        highscoreEL.style.visibility = "visible";
+       
+        //makes start button visible again to play game
+        questionasked.textContent = ("press start to play again");
+        btn.disabled = false;
+        const startScreen = document.getElementById('start')
+        startScreen.classList.remove('hide')
 
-}
+
+    }
 };
